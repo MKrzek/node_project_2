b65ed357225cba269d191cc6f22519992861966d
MKrzek <mkrzek@googlemail.com>
2020-05-07 15:52:22 +0100

    feat: add server and added routes

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res) => {
+  res
+    .status(404)
+    .sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
+});
+
+app.listen(3000, () => {
+  console.log('server running');
+});
