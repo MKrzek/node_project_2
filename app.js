2f8e878b505fb85ec79f336754bf92e3e83ae897
MKrzek <mkrzek@googlemail.com>
2020-05-08 12:18:55 +0100

    feat: add handlebars templates

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const adminRoutes = require('./routes/admin');
+const expressHbs = require('express-handlebars');
+const { adminRouter } = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.engine(
+  'handlebars',
+  expressHbs({
+    defaultLayout: 'main',
+    extname: 'handlebars',
+  })
+);
+app.set('view engine', 'handlebars');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminRouter);
 app.use(shopRoutes);
 
 app.use((req, res) => {
-  res
-    .status(404)
-    .sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
+  res.status(404).render('page-not-found', { pageTitle: 'Page not found' });
 });
 
 app.listen(3000, () => {
