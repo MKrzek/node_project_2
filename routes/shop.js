d9ec18f6d519b6ea5c99ef8e9815eb03f3d3a2f6
MKrzek <mkrzek@googlemail.com>
2020-05-08 12:20:37 +0100

    feat: removed html; implemented hbs templates

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,20 @@
 const express = require('express');
-const path = require('path');
-const rootDir = require('../utils/path');
+
+// const rootDir = require('../utils/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  console.log('adminData', adminData.products);
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop', {
+    products: adminData.products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: adminData.products > 0,
+    activeShop: true,
+  });
 });
 
 module.exports = router;
