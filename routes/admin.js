c2e809906c6120cf0d5fe66f9157e7c2449a1f28
MKrzek <mkrzek@googlemail.com>
2020-05-11 17:17:19 +0100

    feat: add route and view to product edit

--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,7 @@ const adminRoutes = express.Router();
 adminRoutes.get('/add-product', getAddProduct);
 
 adminRoutes.post('/add-product', postAddProduct);
-adminRoutes.put('/edit-product', editProduct);
+adminRoutes.put('/edit-product/:productId(\\d+)', editProduct);
 adminRoutes.get('/products', getAllProducts);
 
 module.exports = adminRoutes;
