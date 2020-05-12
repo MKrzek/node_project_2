61748236e7e56e618676fc67e8dd115d2efc9dd7
MKrzek <mkrzek@googlemail.com>
2020-05-12 16:34:42 +0100

    feat: add methods to cart model

--- models/cart.js
+++ models/cart.js
@@ -9,6 +9,7 @@ const p = path.join(
 
 module.exports = class Cart {
   static addProducts(id, productPrice) {
+    console.log('cart-model', id, productPrice);
     fs.readFile(p, (err, fileContent) => {
       let cart = { products: [], totalPrice: 0 };
       if (!err) {
@@ -32,6 +33,32 @@ module.exports = class Cart {
       fs.writeFile(p, JSON.stringify(cart), err => {
         console.log('cart-errror', err);
       });
+    });
+  }
+
+  static deleteProduct(id, price) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) return;
+
+      const updatedCart = { ...JSON.parse(fileContent) };
+
+      const product = updatedCart.products.find(prod => prod.id === id);
+      if (!product) return;
+      updatedCart.products = updatedCart.products.filter(
+        prod => prod.id !== id
+      );
+      updatedCart.totalPrice -= price * product.qty;
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static getCartProducts(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) cb(null);
+      cb(cart);
     });
   }
 };
