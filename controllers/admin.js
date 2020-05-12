5c6ea67243c058f4971117348229bfc679beec05
MKrzek <mkrzek@googlemail.com>
2020-05-12 16:41:29 +0100

    feat: add controllers for delete and edit product

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,23 +5,50 @@ const getAddProduct = (req, res, next) => {
   res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    productAddCSS: true,
+    productCSS: true,
     activeAddProduct: true,
   });
 };
 
 const postAddProduct = (req, res, next) => {
   const { title, price, imageURL, description } = req.body;
-  const product = new Product(price, title, imageURL, description);
+
+  const product = new Product(null, title, price, imageURL, description);
   product.save();
   res.redirect('/');
 };
 
 const editProduct = (req, res, next) => {
-  res.render('admin/edit-product', {
-    pageTitle: 'Edit Product',
-    path: 'admin/edit-product',
+  const { productId } = req.params;
+  Product.findById(productId, product => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      productCSS: true,
+      product,
+    });
   });
+};
+
+const deleteProduct = (req, res, next) => {
+  const { productId } = req.params;
+  Product.delete(productId);
+  res.redirect('/admin/products');
+};
+const saveEditProduct = (req, res, next) => {
+  const { productId, title, price, imageURL, description } = req.body;
+  const updatedProduct = new Product(
+    productId,
+    title,
+    price,
+    imageURL,
+    description
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
 };
 
 const getAllProducts = (req, res, next) => {
@@ -35,4 +62,12 @@ const getAllProducts = (req, res, next) => {
     });
   });
 };
-module.exports = { getAddProduct, postAddProduct, editProduct, getAllProducts };
+
+module.exports = {
+  getAddProduct,
+  postAddProduct,
+  editProduct,
+  getAllProducts,
+  saveEditProduct,
+  deleteProduct,
+};
