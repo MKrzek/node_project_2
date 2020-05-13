a89ecfa266ae6ad5a8522c9d5a4504f5e443f171
MKrzek <mkrzek@googlemail.com>
2020-05-13 17:28:06 +0100

    fix: removed regex from route params

--- routes/admin.js
+++ routes/admin.js
@@ -14,9 +14,9 @@ const adminRoutes = express.Router();
 adminRoutes.get('/add-product', getAddProduct);
 
 adminRoutes.post('/add-product', postAddProduct);
-adminRoutes.get('/edit-product/:productId(\\d+)', editProduct);
+adminRoutes.get('/edit-product/:productId', editProduct);
 adminRoutes.post('/edit-product', saveEditProduct);
-adminRoutes.get('/delete-product/:productId(\\d+)', deleteProduct);
+adminRoutes.get('/delete-product/:productId', deleteProduct);
 adminRoutes.get('/products', getAllProducts);
 
 module.exports = adminRoutes;
--- routes/shop.js
+++ routes/shop.js
@@ -10,18 +10,19 @@ const {
   deleteCartProduct,
 } = require('../controllers/cart');
 const getCheckout = require('../controllers/checkout');
-const getOrders = require('../controllers/orders');
+const { getOrders, createOrder } = require('../controllers/orders');
 
 const router = express.Router();
 
 router.get('/', getIndex);
-router.get('/products/:productId(\\d+)', getProductDetails);
+router.get('/products/:productId', getProductDetails);
 router.get('/products', getProducts);
 
 router.get('/cart', getCart);
 router.post('/cart', addToCart);
 router.post('/cart-delete-item', deleteCartProduct);
-router.get('/checkout', getCheckout);
+// router.get('/checkout', getCheckout);
 router.get('/orders', getOrders);
+router.post('/create-order', createOrder);
 
 module.exports = router;
