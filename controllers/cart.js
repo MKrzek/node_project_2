d8866bad10208bf5839a68d072c1d13b1f617957
MKrzek <mkrzek@googlemail.com>
2020-05-14 17:28:09 +0100

    refactor: used mongoose schema methods for cart

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,29 +1,36 @@
 const Product = require('../models/product');
+const User = require('../models/user');
 
 const getCart = (req, res, next) => {
-  req.user.getCart().then(cart => {
-    res.render('shop/cart', {
-      pageTitle: 'Your Cart',
-      activeCart: true,
-      products: cart,
-      hasProducts: cart.length > 0,
-      cartCSS: true,
+  User.findById(req.user.id)
+    .lean()
+    .populate('cart.items.productId')
+    .then(user => {
+      const {
+        cart: { items },
+      } = user;
+      console.log('items', items);
+      res.render('shop/cart', {
+        pageTitle: 'Your Cart',
+        activeCart: true,
+        products: items,
+        hasProducts: items.length > 0,
+        cartCSS: true,
+      });
     });
-  });
 };
 
 const addToCart = (req, res, next) => {
   const { productId } = req.body;
   Product.findById(productId)
     .then(product => req.user.addToCart(product))
-    .then(() => {
-      res.redirect('/cart');
-    });
+    .then(result => res.redirect('/cart'));
 };
+
 const deleteCartProduct = (req, res, next) => {
   const { productId } = req.body;
   req.user
-    .deleteFromCart(productId)
+    .removeFromCart(productId)
     .then(() => {
       res.redirect('/cart');
     })
