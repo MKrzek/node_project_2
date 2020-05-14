4639213a1845b803ccb9258063ed6914ff1cefa2
MKrzek <mkrzek@googlemail.com>
2020-05-14 17:27:30 +0100

    feat: refactored order controller to use mongoose schema custom methods

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,14 +1,38 @@
+const Order = require('../models/order');
+const User = require('../models/user');
+
 const createOrder = (req, res, next) => {
   req.user
-    .addOrder()
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      console.log('user', user);
+      const products = user.cart.items.map(i => ({
+        quantity: i.quantity,
+        product: i.productId,
+      }));
+      console.log('products', products);
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user._id,
+        },
+        products,
+      });
+      console.log('order', order);
+      return order.save();
+    })
+    .then(() => req.user.clearCart())
     .then(() => res.redirect('/orders'))
     .catch(err => console.log('order-error', err));
 };
 
 const getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order.find({ 'user.userId': req.user._id })
+    .lean()
+    .populate('products.product')
     .then(orders => {
+      console.log('orders', orders);
       res.render('shop/orders', {
         pageTitle: 'Orders',
         path: '/orders',
