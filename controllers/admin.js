72928bc6bb84858e2afb20b27752a21ff475d62c
MKrzek <mkrzek@googlemail.com>
2020-05-14 13:55:16 +0100

    feat: updated product controller to use mongoose native methods

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,15 +1,17 @@
 const Product = require('../models/product');
 
 const getAllProducts = (req, res, next) => {
-  Product.fetchAll().then(products => {
-    res.render('admin/products', {
-      products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products',
-      hasProducts: products.length > 0,
-      activeAdminProduct: true,
+  Product.find()
+    .lean()
+    .then(products => {
+      res.render('admin/products', {
+        products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products',
+        hasProducts: products.length > 0,
+        activeAdminProduct: true,
+      });
     });
-  });
 };
 
 const getAddProduct = (req, res, next) => {
@@ -24,17 +26,15 @@ const getAddProduct = (req, res, next) => {
 const postAddProduct = (req, res, next) => {
   const { title, price, imageURL, description } = req.body;
 
-  const product = new Product(
+  const product = new Product({
     title,
     price,
     imageURL,
     description,
-    null,
-    req.user._id
-  );
+  });
   product
     .save()
-    .then(result => {
+    .then(() => {
       console.log('Created Product');
       res.redirect('/admin/products');
     })
@@ -45,36 +45,41 @@ const postAddProduct = (req, res, next) => {
 
 const editProduct = (req, res, next) => {
   const { productId } = req.params;
-  Product.findById(productId).then(product => {
-    if (!product) {
-      return res.redirect('/');
-    }
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      productCSS: true,
-      product,
+  Product.findById(productId)
+    .lean()
+    .then(product => {
+      if (!product) {
+        return res.redirect('/');
+      }
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        productCSS: true,
+        product,
+      });
     });
-  });
 };
 
 const saveEditProduct = (req, res, next) => {
   const { productId, title, price, imageURL, description } = req.body;
-  const updatedProduct = new Product(
-    title,
-    price,
-    imageURL,
-    description,
-    productId
-  );
-  updatedProduct.save().then(result => {
+  Product.update(
+    { _id: productId },
+    {
+      title,
+      price,
+      imageURL,
+      description,
+    }
+  ).then(result => {
     res.redirect('/admin/products');
   });
 };
 
 const deleteProduct = (req, res, next) => {
   const { productId } = req.params;
-  Product.deleteById(productId).then(() => res.redirect('/admin/products'));
+  Product.findByIdAndDelete(productId).then(() =>
+    res.redirect('/admin/products')
+  );
 };
 
 module.exports = {
