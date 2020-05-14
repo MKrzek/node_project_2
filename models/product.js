95e90d33fce4633089df98b418e743ee55ffc4f1
MKrzek <mkrzek@googlemail.com>
2020-05-14 13:54:31 +0100

    feat: implement mongoose schema

--- models/product.js
+++ models/product.js
@@ -1,66 +1,12 @@
-const { ObjectId } = require('mongodb');
-const { getDb } = require('../utils/database');
+const mongoose = require('mongoose');
 
-module.exports = class Product {
-  constructor(title, price, imageURL, description, id, userId) {
-    this.title = title;
-    this.price = price;
-    this.imageURL = imageURL;
-    this.description = description;
-    this._id = id ? ObjectId(id) : null;
-    this.userId = userId;
-  }
+const { Schema } = mongoose;
 
-  save() {
-    const db = getDb();
-    let dbOp;
-    if (this._id) {
-      dbOp = db
-        .collection('products')
-        .updateOne({ _id: this._id }, { $set: this });
-    } else {
-      dbOp = db.collection('products').insertOne(this);
-    }
-    return dbOp
-      .then(result => {
-        console.log('result-saving', result);
-      })
-      .catch(err => {
-        console.log('error-saving', err);
-      });
-  }
+const productSchema = new Schema({
+  title: { type: String, required: true },
+  price: { type: Number, required: true },
+  imageURL: { type: String, required: true },
+  description: { type: String, required: true },
+});
 
-  static fetchAll() {
-    const db = getDb();
-    return db
-      .collection('products')
-      .find()
-      .toArray()
-      .then(products => products)
-      .catch(err => {
-        console.log('err', err);
-      });
-  }
-
-  static findById(productId) {
-    const db = getDb();
-
-    return db
-      .collection('products')
-      .findOne({ _id: ObjectId(productId) })
-
-      .then(product => product)
-      .catch(err => {
-        console.log('err', err);
-      });
-  }
-
-  static deleteById(productId) {
-    const db = getDb();
-    return db
-      .collection('products')
-      .deleteOne({ _id: ObjectId(productId) })
-      .then(result => console.log('deleted'))
-      .catch(err => console.log('err', err));
-  }
-};
+module.exports = mongoose.model('Product', productSchema);
