374ce072b7bb2242b86d14304b82497ca4487887
MKrzek <mkrzek@googlemail.com>
2020-05-18 18:04:24 +0100

    feat: add routes for password reset

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,10 @@ const {
   postLogout,
   getSignUp,
   postSignUp,
+  getReset,
+  postReset,
+  getNewPassword,
+  saveNewPassword,
 } = require('../controllers/auth');
 
 const router = express.Router();
@@ -13,5 +17,9 @@ router.post('/login', postLogin);
 router.post('/logout', postLogout);
 router.get('/signup', getSignUp);
 router.post('/signup', postSignUp);
+router.get('/password-reset', getReset);
+router.post('/password-reset', postReset);
+router.get('/password-reset/:resetToken', getNewPassword);
+router.post('/new-password', saveNewPassword);
 
 module.exports = router;
