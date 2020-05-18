40a974a3165fce44526853722ace8ec1238fe7ee
MKrzek <mkrzek@googlemail.com>
2020-05-18 13:12:51 +0100

    fix: we  wre not stroing user name but user email

--- controllers/orders.js
+++ controllers/orders.js
@@ -13,7 +13,7 @@ const createOrder = (req, res, next) => {
 
       const order = new Order({
         user: {
-          name: req.user.name,
+          email: req.user.email,
           userId: req.user._id,
         },
         products,
