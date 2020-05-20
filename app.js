da5b24d4d24ea4b47317e21645fa03e21503364e
MKrzek <mkrzek@googlemail.com>
2020-05-20 16:16:12 +0100

    feat: add packages to create pdf files and parse images

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const path = require('path');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const multer = require('multer');
 const csrf = require('csurf');
 const flash = require('connect-flash');
-
 const expressHbs = require('express-handlebars');
+
 const User = require('./models/user');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -23,6 +24,28 @@ const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions',
 });
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${new Date().toISOString()}-${file.originalname}`);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 const csrfProtection = csrf();
 
 app.engine(
@@ -37,7 +60,9 @@ app.engine(
 app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer({ storage: fileStorage, fileFilter }).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(
   session({ secret: '34d3s', resave: false, saveUninitialized: false, store })
 );
